8618f20cdad8615b0a409c9a3364251f397cda23
bezoerb <ben@sommerlaune.com>
2019-01-19 00:19:55 +0100

    Rewrite

--- index.js
+++ index.js
@@ -1,100 +1,65 @@
-'use strict';
 const postcss = require('postcss');
-const Bluebird = require('bluebird');
-const isString = require('lodash.isstring');
-const defaults = require('lodash.defaults');
 const debug = require('debug')('image-inliner');
-const escape = require('lodash.escaperegexp');
-const last = require('lodash.last');
-const reduce = require('lodash.reduce');
-const filesize = require('filesize');
-const getResource = require('asset-resolver').getResource;
-const getDataUri = require('./lib/image').getDataUri;
+const last = require('lodash/last');
+const escapeRegExp = require('lodash/escapeRegExp');
+const {getDataUriMapping} = require('./lib/image');
 
-module.exports = postcss.plugin('postcss-image-inliner', opts => {
-    opts = defaults(opts || {}, {
-        assetPaths: [],
-        maxFileSize: 10240,
-        b64Svg: false,
-        strict: false
-    });
+const DEFAULTS = {
+  assetPaths: [],
+  maxFileSize: 10240,
+  b64Svg: false,
+  strict: false
+};
 
-    if (isString(opts.assetPaths)) {
-        opts.assetPaths = [opts.assetPaths];
+const loop = cb => {
+  const matcher = /url\(\s*(?:['"]*)(?!['"]*data:)(.*?)(?:['"]*)\s*\)/gm;
+  return decl => {
+    let match;
+    while ((match = matcher.exec(decl.value)) !== null) {
+      cb({decl, url: last(match)});
     }
+  };
+};
 
-    opts.assetPaths.push(process.cwd());
+module.exports = postcss.plugin('postcss-image-inliner', (opts = {}) => {
+  const options = {...DEFAULTS, ...opts};
 
-    function assertSize(resource) {
-        const encoding = resource.mime === 'image/svg+xml' ? 'utf-8' : 'binary';
-        const size = Buffer.byteLength(resource.contents, encoding);
-        if (opts.maxFileSize && opts.maxFileSize < size) {
-            const msg = 'Too big.  ' + filesize(size) + ' exceeds the allowed ' + filesize(opts.maxFileSize);
-            debug(msg);
-            return Bluebird.reject(new Error(msg));
+  if (Array.isArray(options.assetPaths)) {
+    options.assetPaths = [...options.assetPaths, process.cwd()];
+  } else {
+    options.assetPaths = [options.assetPaths, process.cwd()];
+  }
+
+  return async css => {
+    const urls = new Set([]);
+    const filter = /^(background(?:-image)?)|(content)|(cursor)/;
+    // Get urls
+    css.walkDecls(filter, loop(({url}) => urls.add(url)));
+
+    const mapping = await getDataUriMapping([...urls], options);
+
+    css.walkDecls(
+      filter,
+      loop(({decl, url}) => {
+        if (mapping[url]) {
+          const regexp = new RegExp('[\'"]?' + escapeRegExp(url) + '[\'"]?', 'gm');
+          if (url.startsWith('//')) {
+            //       Console.log({url, regexp, val: decl.value});
+          }
+
+          decl.value = decl.value.replace(regexp, '\'' + mapping[url] + '\'');
+          if (url.startsWith('//')) {
+            //       Console.log('RESULT: ', decl.value);
+          }
+
+          debug(url, 'successfully replaced with ', mapping[url]);
+        } else {
+          debug(url, 'failed');
+          if (opts.strict) {
+            throw new Error('No file found for ' + url);
+          }
         }
-
-        return resource;
-    }
-
-    function resolveUrl(filepath) {
-        return getResource(filepath, {
-            base: opts.assetPaths,
-            filter: assertSize
-        }).catch(err => {
-            debug(err.message, filepath, 'could not be resolved');
-        });
-    }
-
-    function loop(cb) {
-        const matcher = /url\(\s*(?:['"]*)(?!['"]*data:)(.*?)(?:['"]*)\s*\)/gm;
-        return function (decl) {
-            let match;
-            while ((match = matcher.exec(decl.value)) !== null) {
-                cb(decl, last(match));
-            }
-        };
-    }
-
-    function compact(data) {
-        return reduce(data, (acc, file, key) => {
-            if (file && file.mime) {
-                acc[key] = file;
-            }
-            return acc;
-        }, {});
-    }
-
-    return function (css) {
-        const replacements = {};
-        const filter = /^(background(?:-image)?)|(content)|(cursor)/;
-        css.walkDecls(filter, loop((decl, url) => {
-            replacements[url] = resolveUrl(url);
-        }));
-
-        return Bluebird.props(replacements)
-            .then(compact)
-            .then(file => {
-                return getDataUri(file, opts);
-            })
-            .then(data => {
-                css.walkDecls(filter, loop((decl, url) => {
-                    if (data[url]) {
-                        const regexp = new RegExp('[\'"]?' + escape(url) + '[\'"]?');
-                        decl.value = decl.value.replace(regexp, '\'' + data[url] + '\'');
-                        debug(url, 'successfully replaced with ', data[url]);
-                    } else {
-                        debug(url, 'failed');
-                        if (opts.strict) {
-                            throw new Error('No file found for ' + url);
-                        }
-                    }
-                }));
-            }).catch(err => {
-                debug(err);
-                return new Bluebird((resolve, reject) => {
-                    reject(err);
-                });
-            });
-    };
+      })
+    );
+  };
 });
